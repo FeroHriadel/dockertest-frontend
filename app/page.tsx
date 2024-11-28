03ce872776943736bfaf0b94ddbd487894ea20f2
Fero Hriadel <ferdinand.hriadel@gmail.com>
2024-11-28 09:10:13 +0100

    frontend ready

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,92 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+import { headers } from 'next/headers';
+import React, { useState, useEffect } from 'react';
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+
+
+interface Item {
+  id: number;
+  name: string;
 }
+
+
+
+const HomePage = () => {
+  const [items, setItems] = useState<Item[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+  const [name, setName] = useState('');
+
+
+  async function getItems() {
+    try {
+      setLoading(true); setError('');
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/items`);
+      const data = await res.json(); if (!data?.data) throw new Error("Got no Items");
+      setItems(data.data);
+    } catch (error) {
+      console.log(error); setError('Error fetching items');
+    }
+  }
+
+  async function deleteItem(id: number) {
+    try {
+      setLoading(true); setError('');
+      const options = { method: 'DELETE', body: JSON.stringify({ id }), headers: { 'Content-Type': 'application/json' } };
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/items`, options);
+      const data = await res.json(); if (!data?.ok) throw new Error("Failed to delete the item");
+      const newItems = items.filter(item => item.id !== id);
+      setItems(newItems);
+    } catch (error) {
+      console.log(error); setError('Error fetching items');
+    }
+  }
+
+  async function createItem() {
+    try {
+      setLoading(true); setError('');
+      const options = { method: 'POST', body: JSON.stringify({ name }), headers: { 'Content-Type': 'application/json' } };
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/items`, options);
+      const data = await res.json(); if (!data?.ok) throw new Error("Failed to create the item");
+      setName('');
+      getItems();
+    } catch (error) {
+      console.log(error); setError('Error fetching items');
+    }
+  }
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    createItem();
+  }
+
+
+  useEffect(() => { getItems(); }, []); //get items on page load
+
+
+  return (
+    <div className='w-[100%] flex flex-col items-center gap-4 mt-20 max-w-[1000px] m-auto'>
+      <h1 className='text-3xl text-center mb-20'>Docker Test Frontend</h1>
+
+      {error && <p className='text-red-500'>{error}</p>}
+
+      {
+        items.map(item => (
+          <div key={item.id} className='w-[50%] flex justify-between'>
+            <h2 className='text-center'>{item.name}</h2>
+            <button onClick={() => deleteItem(item.id)}>DELETE</button>
+          </div>
+        ))
+      }
+
+      <form onSubmit={handleSubmit} className='flex flex-col gap-2 border border-gray-200 mt-40 rounded p-10'>
+        <h2 className='text-xl text-center'>Add Item</h2>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} className='bg-slate-100 rounded' placeholder='Enter item name' />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default HomePage
